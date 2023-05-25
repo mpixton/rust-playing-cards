10b94dcaed5687fd2587c34afd4ddf7b385b8e35
Micah Pixton <micah131313@gmail.com>
2023-05-25 12:41:22 -0600

    DOC(deck): document public api

--- src/deck.rs
+++ src/deck.rs
@@ -3,41 +3,45 @@
 //! At its core, a wrapper around a Vec of [Card]s. Dealing a [Card] removes one
 //! from the top of the Deck and returns it. Shuffling the Deck randomizes the
 //! order. Building a [Deck] is done by calling configuration methods after a
-//! `Deck::new()` call.
-//!
+//! `Deck::custom_new()` call to customize the [Deck] or `Deck::default_new()` to
+//! build a standard French deck.
+//! //!
 //! # Examples
 //! ```
 //! // Create a new full 52 card deck and shuffle it 7 times
-//! let deck = Deck::new().deck_type(DeckType::Full).shuffle(7);
-//! assert_eq!(deck.len(), 52);
+//! let deck = Deck::custom_new().deck_type(DeckType::FullFrench).shuffle(7);
+//! assert_eq!(deck.total_cards(), 52);
 //! ```
 //!
 //! # Todo
 //! - [ ] Update documentation
-//! 
+//!
 
 use crate::card::Card;
 use crate::rank::Rank;
 use crate::suit::Suit;
-use rand::prelude::SliceRandom;
+use rand::seq::SliceRandom;
 use rand::thread_rng;
+use std::collections::VecDeque;
+use std::marker::PhantomData;
 
+/// A deck of playing cards.
 pub struct Deck<T: DeckState> {
-    cards: Vec<Card>,
-    extra: T,
+    cards: VecDeque<Card>,
+    state: PhantomData<T>,
 }
 
+/// Types of [Deck]s.
 pub enum DeckType {
     FullFrench,
 }
 
+/// TypeState trait to control valid states of the [Deck].
 pub trait DeckState {}
 
 struct Start;
 
-struct Building {
-    deck_type: Option<DeckType>,
-}
+struct Building;
 
 struct Shuffling;
 
@@ -49,39 +53,43 @@ impl DeckState for Shuffling {}
 impl DeckState for Finished {}
 
 impl Deck<Start> {
+    /// Begin creation of a custom [Deck].
     pub fn custom_new() -> Deck<Building> {
         Deck {
-            cards: Vec::new(),
-            extra: Building { deck_type: None },
+            cards: VecDeque::new(),
+            state: PhantomData,
         }
     }
 
+    /// Create a default playing card [Deck], shuffled 7 times.
     pub fn default_new() -> Deck<Finished> {
         let deck = Deck {
-            cards: Vec::new(),
-            extra: Building { deck_type: None },
+            cards: VecDeque::new(),
+            state: PhantomData,
         };
+
         deck.deck_type(DeckType::FullFrench).shuffle(7)
     }
 }
 
 impl Deck<Building> {
+    /// Configure the [Deck] as a provided custom [DeckType].
     pub fn deck_type(self, deck_type: DeckType) -> Deck<Shuffling> {
         let deck_size = match deck_type {
             DeckType::FullFrench => 52,
         };
 
-        let mut cards: Vec<Card> = Vec::with_capacity(deck_size);
+        let mut cards: VecDeque<Card> = VecDeque::with_capacity(deck_size);
 
         for suit in Suit::VALUES.iter() {
             for rank in Rank::VALUES.iter() {
-                cards.push(Card::new(*rank, *suit))
+                cards.push_back(Card::new(*rank, *suit))
             }
         }
 
         Deck {
             cards,
-            extra: Shuffling,
+            state: PhantomData,
         }
     }
 }
@@ -90,7 +98,7 @@ impl Deck<Shuffling> {
     /// Shuffles the [Deck] anywhere from 1 to 10 times.
     pub fn shuffle(self, shuffles: usize) -> Deck<Finished> {
         let mut cards = self.cards;
-        let mut shuffling = || cards.shuffle(&mut thread_rng());
+        let mut shuffling = || cards.make_contiguous().shuffle(&mut thread_rng());
 
         match shuffles {
             1..=10 => {
@@ -101,22 +109,31 @@ impl Deck<Shuffling> {
             _ => shuffling(),
         }
 
-        let (first, last) = cards.split_at(cards.len() / 2);
+        let halfway = cards.len() / 2;
 
-        cards = [last, first].concat();
+        let (first, last) = cards.make_contiguous().split_at(halfway);
+
+        let cards: VecDeque<Card> = [last, first].concat().into();
 
         Deck {
             cards,
-            extra: Finished,
+            state: PhantomData,
         }
     }
 }
 
 impl Deck<Finished> {
+    /// Deals the top [Card] from the [Deck].
     pub fn deal_top_card(&mut self) -> Option<Card> {
-        self.cards.pop()
+        self.cards.pop_front()
     }
 
+    /// Deals the bottom [Card] from the [Deck].
+    pub fn deal_bottom_card(&mut self) -> Option<Card> {
+        self.cards.pop_back()
+    }
+
+    /// Get the all [Card]s left in the [Deck].
     pub fn total_cards(&self) -> usize {
         self.cards.len()
     }
